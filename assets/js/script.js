b1f34d1028278496358bdc75660be2c075a10555
forbegos <forbegos@me.com>
2021-05-03 19:05:26 -0600

    weather data api data extract and post working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,58 @@
 // - declarations -
 
 var cities = new Array();
+var input = "";
+var city = {
+  name: "",
+  temp: "",
+  wind: "",
+  humidty: "",
+  uVIndex: 0.0,
+};
 
-function search() {
-  var input = $("#formCityInput").val();
+function handleSearch() {
+  $("li").remove();
+
+  input = $("#formCityInput").val();
   cities.push(input);
   console.log(cities);
   $("#search-history").append("<button>" + input + "</button>");
   $("button").addClass("list-group-item");
+
+  searchApi(input);
 }
 
-$("#search-btn").on("click", search);
+function searchApi(input) {
+  var weatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    input +
+    "&units=imperial&appid=c2e67013d929cf413079d489df0a2fe1";
+
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      postResults(data);
+    });
+}
+
+function postResults(data) {
+  var wDate = new Date(data.dt * 1000).toLocaleDateString("en-US");
+  city.name = input;
+  city.temp = data.main.temp;
+  city.wind = data.wind.speed;
+  city.humidity = data.main.humidity;
+  city.uVIndex = "UVINDEX";
+  console.log(city);
+
+  $("#main-output").append("<li> <h2>" + city.name + "</h2></li>");
+  $("#main-output").append("<li> <p>Temp: " + city.temp + "°F</p></li>");
+  $("#main-output").append("<li> <p>Wind: " + city.wind + "</p></li>");
+  $("#main-output").append("<li> <p>Humidity: " + city.humidity + "</p></li>");
+  $("#main-output").append("<li> <p>UV Index: " + city.uVIndex + "</p></li>");
+  $("li").addClass("list-group-item");
+}
+
+$("#search-btn").on("click", handleSearch);
